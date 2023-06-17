0394ed668617b5e0af0ffff938ad594dcde99c91
Yoiner Duran <duranyoiner@gmail.com>
2023-06-17 00:31:54 -0500

    video 140

--- config/views.py
+++ config/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from apps.store.models import Product
+from django.views.generic import TemplateView
 
-def home(request):
-  return render(request,'home.html')
+class HomeView(TemplateView):
+  template_name = 'home.html'
+
+  def get_context_data(self, **kwargs):
+      context = super().get_context_data(**kwargs)
+      products=Product.objects.all().filter(is_available=True)
+      context["products"] = products
+      return context
