f338dfadc509cfe56ee5f9fd0fa3bbbe5c4cb02f
Yoiner Duran <duranyoiner@gmail.com>
2023-06-16 20:45:21 -0500

    config de azure storage

--- config/production.py
+++ config/production.py
@@ -21,7 +21,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 conn_str = os.environ['AZURE_MYSQL_CONNECTIONSTRING']
--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -112,9 +113,32 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static')]
+#media file
+# MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+# no olvidar porner el MEDIA_URL al upload_to del modelo con archivo
+MEDIA_URL='media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#configuracion para django azure storage
+if 'WEBSITE_HOSTNAME' in os.environ: 
+    azure_storage_blob = os.environ['AZURE_STORAGE_BLOB']
+    azure_storage_blob_parametros = {parte.split(' = ')[0]:parte.split(' = ')[1] for parte in azure_storage_blob.split('  ')}
+else:
+    azure_storage_blob_parametros = {'account_name':os.environ.get('ACCOUNT_NAME'),
+                                     'container_name':os.environ.get('CONTAINER_NAME'),
+                                     'account_key':os.environ.get('ACCOUNT_KEY')}
+
+AZURE_CONTAINER = azure_storage_blob_parametros['container_name']
+AZURE_ACCOUNT_NAME = azure_storage_blob_parametros['account_name']
+AZURE_ACCOUNT_KEY = azure_storage_blob_parametros['account_key']
+STORAGES = {
+    "default": {"BACKEND": "storages.backends.azure_storage.AzureStorage"},
+    "staticfiles": {"BACKEND": "custom_storage.custom_azure.PublicAzureStaticStorage"},
+    "media": {"BACKEND": "custom_storage.custom_azure.PublicAzureMediaStorage"},
+}
