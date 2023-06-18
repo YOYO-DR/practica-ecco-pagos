c3b3bc2bcc0656af91b50bd5f1aacd16c616383c
Yoiner Duran <duranyoiner@gmail.com>
2023-06-18 18:19:29 -0500

    video 163

--- apps/store/views.py
+++ apps/store/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import TemplateView
-
 from apps.carts.models import CartItem
 from apps.carts.views import _cart_id
 from .models import Product
 from apps.category.models import Category
+from django.core.paginator import EmptyPage,PageNotAnInteger, Paginator
 # Create your views here.
 
 class StoreView(TemplateView):
@@ -12,12 +12,18 @@ class StoreView(TemplateView):
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        if self.kwargs.get('category_slug'):
-            categories = get_object_or_404(Category,slug=self.kwargs.get('category_slug'))
-            context["products"] = Product.objects.filter(is_available=True,category=categories)
+        if self.kwargs.get('category_slug'): #obtengo el slug de la categoria
+            categories = get_object_or_404(Category,slug=self.kwargs.get('category_slug')) #obtengo la categoria y si no existe, retorno un 404 not found
+
+            products = Product.objects.filter(is_available=True,category=categories)# obtengo los productos disponibles y que sean de la categoria buscada
         else:
-            context["products"] = Product.objects.filter(is_available=True)
-        context["product_count"] = context["products"].count()
+            products = Product.objects.filter(is_available=True) #traigo todas las categorias
+            
+        paginator = Paginator(products, 6) # creo el paginador, le paso el query, y la cantidad en la cual quiero dividirlos
+        page = self.request.GET.get('page') # obtengo el numero de la pagina a accedes
+        context['products']=paginator.get_page(page) # le paso los productos, el cual cuando lo recorra, accedere a los productos normalmente, pero el "products" contendra toda la informacion del paginador para poder utilizarlo
+
+        context["product_count"] = products.count() # le paso la cantidad de productos encontrados
         return context
 
 class ProductDetailView(TemplateView):
