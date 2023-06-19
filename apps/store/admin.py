7ef76f22d4153598a8a149db96b8a04f69ebc028
Yoiner Duran <duranyoiner@gmail.com>
2023-06-19 16:14:11 -0500

    video 166

--- apps/store/admin.py
+++ apps/store/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product,Variation
 
 class ProductAdmin(admin.ModelAdmin):
     list_display=('product_name','price','stock','category','modified_date','is_available')
     prepopulated_fields={'slug':('product_name',)}
 
-# Register your models here.
+class VariationAdmin(admin.ModelAdmin):
+    list_display=('product','variation_category','variation_value','is_active')
+
+    # para decirle que valores se pueden editar en la previsualización
+    list_editable = ('is_active',)
+
+    #para agregar un filtro segun estos datos
+    list_filter=('product','variation_category','variation_value','is_active')
+
+# primer valor el modelo y luego el modelo de administración
 admin.site.register(Product,ProductAdmin)
+admin.site.register(Variation,VariationAdmin)
--- apps/store/models.py
+++ apps/store/models.py
@@ -22,7 +22,33 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
-    
-    
 
+# para las funciones para retornarlos valores segun el filtro que se haga
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager,self).filter(variation_category='color',is_active=True)
+    
+    def tallas(self):
+        return super(VariationManager,self).filter(variation_category='talla',is_active=True)
+
+variation_category_choice = (
+    ('color','color'),
+    ('talla','talla')
+)
+
+# para guardar las variantes de los productos
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    # los diferentes valores de los productos
+    variation_category=models.CharField(max_length=100,choices=variation_category_choice)
+    variation_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_date=models.DateTimeField(auto_now=True)
+
+    # relaciono el modelo con el manager para que los modelos puedan accedes a las 2 funciones
+    objects=VariationManager()
+
+    def __str__(self):
+        return self.product.product_name
 
