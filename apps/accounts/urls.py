4b2228c1bd6e550e577f228c91633704da310fdc
Yoiner Duran <duranyoiner@gmail.com>
2023-06-22 13:29:04 -0500

    cambio contraseña beta v1

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('logout/',logout,name='logout'),
     path('dashboard/',dashboard,name='dashboard'),
     path('',dashboard,name='dashboard'),
+    path('forgotPassword/',forgotPassword,name='forgotPassword'),
+    path('resetPassword_validate/<uidb64>/<token>/',resetPassword_validate,name='resetPassword_validate'),
+    path('resetPassword/',resetPassword,name='resetPassword'),
     path('activate/<uidb64>/<token>/',activate,name='activate'),
 ]
 
