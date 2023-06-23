f78bfc9f924121805c912adeae5962f31334052d
Yoiner Duran <duranyoiner@gmail.com>
2023-06-22 19:54:08 -0500

    arreglando bug de cambio de contraseña

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -11,6 +11,7 @@ from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.encoding import force_bytes
 from django.core.mail import EmailMessage
+from django.views.generic import TemplateView
 
 # crear contraseña para aplicacion https://support.google.com/accounts/answer/185833?sjid=11686343906281568208-NA
 
