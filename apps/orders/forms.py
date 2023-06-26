c8f8081324a37642d1131929f53397e8dc342516
Yoiner Duran <duranyoiner@gmail.com>
2023-06-25 19:29:30 -0500

    video 189

---
+++ apps/orders/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+from .models import Order
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields=['first_name', 'last_name', 'email', 'addres_line_1','addres_line_2','phone','country','city','state','order_note']
--- config/urls.py
+++ config/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('store/',include('apps.store.urls')),
     path('cart/',include('apps.carts.urls')),
     path('accounts/',include('apps.accounts.urls')),
+    path('orders/',include('apps.orders.urls')),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
